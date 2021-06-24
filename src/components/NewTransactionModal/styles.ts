ba12d74d2e89dda65d96eb25304c5ccea0608818
pedronazarito98 <57152268+pedronazarito98@users.noreply.github.com>
2021-06-24 15:16:58 -0300

    updated

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -38,7 +38,7 @@ export const Container = styled.form`
             border:0;
             font-size: 1rem;
             margin-top: 1.5rem;
-            font-weight:  600;;
+            font-weight:  600;
 
             transition: filter 0.2s;
 
